8e6f2b4b6cef60423530962c195ae872aa46bf7a
Sujal <sujalvarsadiya9018@gmail.com>
2024-10-03 13:58:18 +0530

    Done !!

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import PortFolio from './components/PortFolio'
+import Experiance from './components/Experiance'
+import Footer from './components/Footer'
+import Contact from './components/Contact'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Home />
+        <About />
+        <PortFolio />
+        <Experiance />
+        <Contact />
+        <Footer />
+      </div>
+      <Toaster/>
+    </>
+  )
+}
+
+export default App
